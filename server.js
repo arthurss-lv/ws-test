13f34cb4cd973fedd454f7974a16c48afef0acf6
arthurss-lv <114364754+arthurss-lv@users.noreply.github.com>
2023-02-22 16:09:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,3 +10,19 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+
+const { Server } = require('ws');
+
+const wss2 = new Server({ app });
+
+wss2.on('connection', function (ws) {
+  ws.on('message', function (message) {
+    console.log('received: %s', message)
+  })
+
+  setInterval(
+    () => ws.send(`${new Date()}`),
+    1000
+  )
+})
